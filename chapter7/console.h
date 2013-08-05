99175b03796b6330e912d5b0d3e4bcd48044c105
Ying-Shiuan Pan <yingshiuan.pan@gmail.com>
2013-08-05 23:30:22 +0800

    ch7: copy x64 files from chapter6

--- chapter7/console.h
+++ chapter7/console.h
@@ -1,7 +1,14 @@
 #include <stdint.h>
-#include <hypercall-x86_32.h>
 #include <xen/xen.h>
 #include <xen/io/console.h>
+
+#if defined(__i386__)
+#include <hypercall-x86_32.h>
+#elif defined(__x86_64__)
+#include <hypercall-x86_64.h>
+#else
+#error "Unsupported architecture"
+#endif
 
 int console_init(start_info_t * start);
 int console_write(char * message);
